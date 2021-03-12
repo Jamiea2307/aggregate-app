2c7a3568ef373be64d5e23f3630a08ac0ae8f345
Jamie <ackland2307@gmail.com>
2021-03-12 16:38:22 +0000

    backend added

--- Back-end/routes/redditStories.js
+++ Back-end/routes/redditStories.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET home page. */
-router.get("/", (req, res, next) => {
-  res.send("Home Page");
+router.get("/getStories", (req, res, next) => {
+  res.send("redditStories");
 });
 
 module.exports = router;
